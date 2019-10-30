aecea737088d49d5eefc32c1c172df33f3ca00c7
System Administrator <root@Bijoys-MacBook-Pro.local>
2019-10-30 15:17:17 +0100

    creating reducers

---
+++ src/reducers/gets/reducer.test.js
@@ -0,0 +1,22 @@
+import { types } from './../../actions/types';
+import getsReducer from './reducer';
+
+describe('Posts Reducer', () => {
+
+    it('Should return default state', () => {
+        const newState = getsReducer(undefined, {});
+        expect(newState).toEqual([]);
+    });
+
+    it('Should return new state if receiving type', () => {
+
+        const gets = [{ title: 'Test 1'}, { title: 'Test 2'}, { title: 'Test 3'}];
+        const newState = getsReducer(undefined, {
+            type: types.GET_MOVIES,
+            payload: gets
+        });
+        expect(newState).toEqual(gets);
+
+    });
+
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
-import success from './successReducer';
+import gets from './gets/reducer';
 
 export default combineReducers({
-    success
+    gets
 })
